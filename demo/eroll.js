7972dbef394c442756a67a5c3c9ea2bc82d0c665
lsj <lsj9383@163.com>
2017-07-09 00:28:45 +0800

    update css and readme

--- demo/eroll.js
+++ demo/eroll.js
@@ -1,10 +1,10 @@
 var eroll = {};
 
 eroll.init = function(rollid, option){
-    var mainroll = $(rollid);
-    for(var groupNum=0; groupNum<option.length; groupNum++){
-        words = option[groupNum];
-        var new_roll_face = "<div class='roll-face'><div class='roll-words'>";
+    var mainroll = $(rollid);			//滑动块所处的顶层div
+    for(var groupNum=0; groupNum<option.length; groupNum++){		//读取option中的二维数组
+        words = option[groupNum];									//滑动块中所存在的数字
+        var new_roll_face = "<div class='roll-face'><div class='roll-words'>";	//添加滑动块以及滑动块所使用的数字
         for(var wordNum=0; wordNum<words.length; wordNum++){
             new_roll_face += "<div class='word'>"+words[wordNum]+"</div>";
         }
@@ -13,9 +13,9 @@ eroll.init = function(rollid, option){
     }
 
     var erollObject = new Object();
-    erollObject.id = rollid;
-    erollObject.op = option;
-    erollObject.roll = function(groupNum, value){
+    erollObject.id = rollid;		//记录当前初始化的滑动块的顶层div
+    erollObject.op = option;		//记录当前初始化滑动块的配置
+    erollObject.roll = function(groupNum, value){		//指定第几个滑动块滑动的大小，以px为单位
         if(value<0){
             var strValue = Math.abs(value)+"px";
         }else{
@@ -24,14 +24,14 @@ eroll.init = function(rollid, option){
         $(this.id+" .roll-face").eq(groupNum).find(".roll-words").animate({top: strValue}, 'slow', 'swing', function(){});
     }
 
-    erollObject.roll2index = function(groupNum, index){
+    erollObject.roll2index = function(groupNum, index){	//指定第几个滑动块滑动到第几个元素
         var strWordHeight = $(this.id+" .roll-face").eq(groupNum).find(".roll-words .word").eq(0).css('height');
         var wordHeight = parseInt(strWordHeight);
         var value = wordHeight * index;
         this.roll(groupNum, value);
     }
 
-    erollObject.roll2string = function(str){
+    erollObject.roll2string = function(str){			//将滑动块设置为字符串显示的形式
         for(var wordNum=0; wordNum<str.length; wordNum++){
             var words = this.op[wordNum];
             var index = -1;
--- readme.md
+++ readme.md
@@ -1,10 +1,44 @@
 ﻿# EROLL
 一款web前端的字符滑动组件,下面是显示效果：
 <p align="center">
-  <img src="https://raw.githubusercontent.com/lsj9383/LispJava/master/icon/title.jpg?raw=true">
+  <img src="https://raw.githubusercontent.com/lsj9383/eroll/master/demo.gif">
 </p>
 
 ## 一、最小示例
+### 1.引入相关文件
+在`head`标签中加入所需要的css和js文件。
+* eroll.css，该文件是滑动效果的基本布局。
+* jquery.min.js，该文件是jquery。
+* animateBackground-plugin.js，该文件提供动画支持。
+* eroll.js, 该文件是实现滑动效果的核心js代码。
+```html
+<link rel="stylesheet" href="eroll.css">
+<script type='text/javascript' src='jquery.min.js'></script>
+<script type="text/javascript" src="animateBackground-plugin.js"></script>
+<script type="text/javascript" src="eroll.js"></script>
+```
+### 2.运行
+```html
+<body>
+	<div id="eroll"></div>
+	
+	<script type="text/javascript">
+		var ro = eroll.init("#eroll", [
+                    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],			//第一个滑动块提供的字符
+                    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 		//第二个滑动块提供的字符
+                    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]);		//第三个滑动块提供的字符
+		ro.roll2string("123");
+		ro.roll2string("234");
+		ro.roll2string("345");
+	</script>
+</body>
+```
+该最小示例没有为显示设置样式，因此采用的是默认样式。
 
+## 二、样式定义
 
-## 二、API
+## 三、API
+### 1.init
+### 2.roll
+### 3.roll2index
+### 4.roll2string
